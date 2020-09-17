38bbd3f1efe794de4bad55bc22a50704e7644e29
Amir ullah <70256195+amirullah213@users.noreply.github.com>
2020-09-17 16:05:41 +0500

    changes in css

--- src/app/core/modules/shared-module/shared.module.ts
+++ src/app/core/modules/shared-module/shared.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SidrbarComponent } from './sidrbar/sidrbar.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
-  declarations: [NavComponent, FooterComponent,HeaderComponent ],
-  exports: [NavComponent,  FooterComponent,HeaderComponent ]
+  declarations: [NavComponent, FooterComponent,HeaderComponent, SidrbarComponent ],
+  exports: [NavComponent, FooterComponent, HeaderComponent, SidrbarComponent ]
 })
 
 export class SharedModule { }
---
+++ src/app/core/modules/shared-module/sidrbar/sidrbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidrbar',
+  templateUrl: './sidrbar.component.html',
+  styleUrls: ['./sidrbar.component.css']
+})
+export class SidrbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
